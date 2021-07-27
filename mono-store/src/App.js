8f5a24289734c47ca77479d4440077a52f01787f
aaronm-2112 <aaron.marroquin96@gmail.com>
2021-07-27 08:29:02 -0700

    firebase auth integration and config added to the client

--- mono-store/src/App.js
+++ mono-store/src/App.js
@@ -3,19 +3,42 @@ import Storefront from "./pages/Storefront/Storefront.component";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/header/header.component";
 import SigninAndSignUp from "./pages/signin-and-signup/signin-and-signup.component";
+import React from "react";
+import Auth from "./firebase/config";
 
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <Router>
-        <Switch>
-          <Route exact path="/storefront" component={Storefront}></Route>
-          <Route exact path="/signin" component={SigninAndSignUp}></Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: null,
+    };
+  }
+
+  componentDidMount() {
+    Auth.createUserWithEmailAndPassword(
+      "aaron.marroquin96@gmail.com",
+      "123123"
+    ).then((userCredential) => {
+      this.setState({ user: userCredential.user }, () => {
+        console.log(this.state);
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header></Header>
+        <Router>
+          <Switch>
+            <Route exact path="/storefront" component={Storefront}></Route>
+            <Route exact path="/signin" component={SigninAndSignUp}></Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ mono-store/src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+// firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDWTbJi6L33PTLhx7xxFUGTrkGDQZ1Fhrs",
+  authDomain: "mono-store-a0e25.firebaseapp.com",
+  projectId: "mono-store-a0e25",
+  storageBucket: "mono-store-a0e25.appspot.com",
+  messagingSenderId: "732185126715",
+  appId: "1:732185126715:web:f5839a1a83da640acfb91e",
+  measurementId: "G-LE12X9V1SL",
+};
+
+// get the firebase client
+const firebaseClient = firebase.initializeApp(firebaseConfig);
+
+// get the auth object out of the firebase client
+const Auth = firebaseClient.auth();
+
+// export it for use elsewhere
+export default Auth;
