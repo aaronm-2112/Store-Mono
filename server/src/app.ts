3e8179ab7f24e63bb84f85a8be819e5e7194ee31
aaronm-2112 <aaron.marroquin96@gmail.com>
2021-07-29 15:39:42 -0700

    Node backend now serves the React application.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from "express";
+import * as path from "path";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
+// treat the react application's build folder as the rppt directory to serve static React assets
+app.use(express.static(path.resolve(__dirname, "../../client/build")));
+
+// api routes
+app.get("/api/v1/*", (req: Request, res: Response) => {
   return res.sendStatus(201);
+});
+
+// all non api routes go to the React app
+app.get("*", (req: Request, res: Response) => {
+  return res.sendFile(
+    path.join(__dirname, "../../client", "build", "index.html")
+  );
 });
 
 export default app;
